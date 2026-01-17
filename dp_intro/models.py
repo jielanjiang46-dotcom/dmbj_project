dded200594d4cdd83a2afbd862338c4cfbf22f36
Lyra <jielanjiang46@gmail.com>
2026-01-17 22:39:55 +0800

    create topic and entry

--- dp_intro/models.py
+++ dp_intro/models.py
@@ -9,3 +9,14 @@ class Topic(models.Model):
     def __str__(self):
         """返回模型的字符串表示"""
         return self.text
+
+class Entry(models.Model):
+    topic=models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text=models.TextField()
+    date_added=models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        verbose_name_plural='entries'
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
